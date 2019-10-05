16e65ffab1276f2957f09b82197c721ac5d1ab27
allison cortez <allizetorc@gmail.com>
2019-10-05 19:45:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,6 +10,18 @@ end
 return reversed_string.join(' ')
 end
 
+def reverse_each_word(string)
+string = string.split(' ')
+reversed_string = []
+
+string.collect do |word|
+  reversed_string.push(word.reverse)
+  
+end
+ 
+return reversed_string.join(' ')
+end
+
 
 
 
