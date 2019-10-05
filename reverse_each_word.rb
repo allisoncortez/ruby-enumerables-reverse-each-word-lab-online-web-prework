b8097e4451747e22181335723dad66e33147a230
allison cortez <allizetorc@gmail.com>
2019-10-05 19:45:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -22,21 +22,3 @@ end
 return reversed_string.join(' ')
 end
 
-
-
-
-
-
-
-
-
-
-
-
-# def reverse_each_word(string)
-#   list = string.split(" ")
-#   list.each do |word|
-#     word.reverse
-#     return list.collect
-#   end
-# end
